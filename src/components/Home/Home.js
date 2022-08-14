e706b0afd0e0fbbd7565eed42fd1813501e86a4a
Muhammad Enamul Haque <enahaque1164@gmail.com>
2022-08-15 01:31:14 +0600

    no 2 job complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import './Home.css';
 
 
 const Home = () => {
     return (
-        <div>
-           
-            <h1>Home</h1>
+        <div className='container'>
+
+            <div className='text-container'>
+                <h1>Your Next Laptop</h1>
+                <h1>Your First Choose</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore ratione pariatur consequuntur non. Hic doloremque distinctio ex deleniti, voluptatum officia quae veniam ea expedita fugiat pariatur perferendis, eum consequatur similique.</p>
+                <button className='btn'>Buy Now</button>
+            </div>
+
+            <div className='image-container'>
+                <img src="https://i.ibb.co/6m0bzxt/laptop.png" alt="" />
+            </div>
+
         </div>
     );
 };
