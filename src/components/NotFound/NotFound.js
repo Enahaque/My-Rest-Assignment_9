b020f0de34b426ddb0f7b212acbada354a117a78
Muhammad Enamul Haque <enahaque1164@gmail.com>
2022-08-14 23:33:47 +0600

    first commit

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h5>The page you're looking notfound</h5>
+            <h1>404!!</h1>
+        </div>
+    );
+};
+
+export default NotFound;
