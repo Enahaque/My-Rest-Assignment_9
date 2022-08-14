4931be4f278d053431eca31697bb37a5a8cbdd17
Muhammad Enamul Haque <enahaque1164@gmail.com>
2022-08-15 03:14:25 +0600

    updated

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Blogs.css';
 
 const Blogs = () => {
     return (
         <div>
-            <h1>Hey blogs</h1>
+            <div className='first-div'>
+                
+                <h2>What is context API?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque necessitatibus mollitia architecto non ab nulla repudiandae quisquam aliquid consectetur quae, quia, explicabo voluptates omnis sint aperiam delectus quibusdam, nobis id?</p>
+
+            </div>
+
+            <div className='second-div'>
+
+                <h2>What is context API?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque necessitatibus mollitia architecto non ab nulla repudiandae quisquam aliquid consectetur quae, quia, explicabo voluptates omnis sint aperiam delectus quibusdam, nobis id?</p>
+
+            </div>
+
+            <div className='last-div'>
+
+                <h2>What is context API?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque necessitatibus mollitia architecto non ab nulla repudiandae quisquam aliquid consectetur quae, quia, explicabo voluptates omnis sint aperiam delectus quibusdam, nobis id?</p>
+
+            </div>
         </div>
     );
 };
